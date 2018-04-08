f94fbd2e2b5d296e1165ce827c8ccc13022a6b0f
arronZhu <1274294199@qq.com>
2018-04-08 18:18:30 +0800

    update

--- js/searchList.js
+++ js/searchList.js
@@ -1,9 +1,9 @@
 $(function(){
     $(".show-more").click(function(){
        
-        var flag=true;
+        var flag=false;
         if(this.flag){
-       $("content-body").css("overflow","visible").css("height","auto");
+       $(".content-body").css("overflow","visible").css("height","auto");
        $(this).children().removeClass("am-icon-angle-down");
        $(this).children().addClass("am-icon-angle-up"); 
        this.flag = false;
@@ -11,7 +11,7 @@ $(function(){
     }
     
     else{
-        $(this).parent().siblings().css("overflow","hidden").css("height","30px"); 
+        $(".content-body").css("overflow","hidden").css("height","30px"); 
         $(this).children().removeClass("am-icon-angle-up");
         $(this).children().addClass("am-icon-angle-down"); 
         this.flag=true;
