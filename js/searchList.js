55e5c6dd785db115a929d6afa4a4dca598fd1912
Arron <1274294199@qq.com>
2018-04-08 23:45:32 +0800

    更新展开隐藏orz

--- js/searchList.js
+++ js/searchList.js
@@ -1,20 +1,17 @@
-$(function(){
-    $(".show-more").click(function(){
-       
-        var flag=false;
-        if(this.flag){
-       $(".content-body").css("overflow","visible").css("height","auto");
-       $(this).children().removeClass("am-icon-angle-down");
-       $(this).children().addClass("am-icon-angle-up"); 
-       this.flag = false;
-       console.log(flag);
-    }
-    
-    else{
-        $(".content-body").css("overflow","hidden").css("height","30px"); 
-        $(this).children().removeClass("am-icon-angle-up");
-        $(this).children().addClass("am-icon-angle-down"); 
-        this.flag=true;
-    }
+$(function () {
+    // 列表展开隐藏orz..orz QaQ
+    $(".show-more").click(function () {
+        if ($(this).siblings(".content-body").hasClass("show-content-body")) {
+            $(this).siblings(".content-body").removeClass("show-content-body");
+            $(this).children().removeClass("am-icon-angle-down");
+            $(this).children().addClass("am-icon-angle-up");
+        } else {
+            $(this).children().removeClass("am-icon-angle-up");
+            $(this).children().addClass("am-icon-angle-down");
+            $(this).siblings(".content-body").addClass("show-content-body");
+
+        }
     });
+    // 已选条件展示orz
+    
 })
