cd1fce7a2a537de79fddbb2e49b02669bc98e579
arronZhu <1274294199@qq.com>
2018-04-09 11:20:07 +0800

    update footer

--- js/searchList.js
+++ js/searchList.js
@@ -13,5 +13,19 @@ $(function () {
         }
     });
     // 已选条件展示orz
-    
+        each_click_items = $('.sx_child');
+
+        each_click_items.each(function(i){
+
+     $(this).click(function(){
+            var selected_items_value=$(this).text();
+             var formatted_strings="<span>" + selected_items_value +"<i></i></span>";
+             $(".selected-box").append(formatted_strings).children().addClass("selected-items");
+             $(".selected-box").append(formatted_strings).children().siblings("i").addClass("am-icon-close");
+             $(this).parent().parent(".am-cf").css("display","none");
+
+      })
+
+  });
+
 })
