090afa1d403d8943bfd04e76cc4558f5f3ba8fe0
arronZhu <1274294199@qq.com>
2018-04-09 18:15:02 +0800

    update

--- js/searchList.js
+++ js/searchList.js
@@ -13,19 +13,47 @@ $(function () {
         }
     });
     // 已选条件展示orz
-        each_click_items = $('.sx_child');
+    each_click_items = $('.sx_child');
 
-        each_click_items.each(function(i){
+    each_click_items.each(function (i) {
 
-     $(this).click(function(){
-            var selected_items_value=$(this).text();
-             var formatted_strings="<span>" + selected_items_value +"<i></i></span>";
-             $(".selected-box").append(formatted_strings).children().addClass("selected-items");
-             $(".selected-box").append(formatted_strings).children().siblings("i").addClass("am-icon-close");
-             $(this).parent().parent(".am-cf").css("display","none");
+        $(this).click(function () {
+            var selected_items_value = $(this).text();
+            var formatted_strings = "<span>" + selected_items_value + "<i></i></span>";
+            $(".selected-box").append(formatted_strings).children().addClass("selected-items");
+            $(".selected-box").children().children(':last-child').addClass("am-icon-close");
+            $(this).parent().parent(".am-cf").css("display", "none");
+            var target_css=$(this).parent().parent(".am-cf");
 
-      })
+            click_items = $('.selected-items');
+            click_items.each(function (i) {
+                $(this).click(function () {
+                    var selected_item_text = $(this).text();
+                    console.log(selected_item_text);
+                    var target_value=$('.sx_child').text();
+                    console.log(target_value);
+                    target_css.css("display", "block");
+                    $(this).remove();
 
-  });
+                });
+
+            });
+
+        });
+
+
+    });
+    // 
+    //   反选内容
+    // reverse_click_items = $('.selected-items');
+    // reverse_click_items.each(function (i) {
+
+    //     $(this).click(function () {
+    //         console.log(reverse_click_items);
+    //         $(this).remove();
+
+
+    //     });
+    // });
 
 })
