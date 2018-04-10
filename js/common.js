1fb11fa76a2f7a61cb6e155d2524b6198ef5aaa9
shaossss <969348836@qq.com>
2018-04-10 17:54:16 +0800

    update

--- js/common.js
+++ js/common.js
@@ -1,10 +1,3 @@
 
 
-$(function(){
-    // 高度计算示例
-    var docHeight = $(document).height();
-    winHeight = $(window).height();
-    headerHeight = $(".header").outerHeight();
-    footerHeight = $(".footer").outerHeight();
-    $("div.content-container").height(winHeight-headerHeight-footerHeight);
-})
+
