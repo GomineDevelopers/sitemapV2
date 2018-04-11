c06191da9891531ed6808d0eae50eeec0fd9b278
shaossss <969348836@qq.com>
2018-04-11 17:44:49 +0800

    update

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
-
+ function formatDate (date, showDetail) {
+    var isShow = showDetail || false;
+    var d = new Date(parseInt(date) * 1000)
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var date1 = d.getDate();
+    var hour = d.getHours();
+    var minute = d.getMinutes();
+    var second = d.getSeconds();
+    if (isShow)
+        return year + "-" + month + "-" + date1 + " " + hour + ":" + minute + ":" + second;
+    else
+        return year + "-" + month + "-" + date1;
+}
 
 
