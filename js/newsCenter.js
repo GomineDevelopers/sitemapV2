226a56db3b29cf6c4772659eaaf5529a196070cc
shaossss <969348836@qq.com>
2018-04-12 17:06:31 +0800

    update

--- js/newsCenter.js
+++ js/newsCenter.js
@@ -1,6 +1,7 @@
 var newsCenter = new Vue({
     el:'#main',
     data:{
+        loading: false,
         newsLeftData:[],
         newsRightData:[]
     },
@@ -9,12 +10,34 @@ var newsCenter = new Vue({
         axios.all([getLeft(), getRight()])
             .then(axios.spread(function (leftC, rightC) {
                 // 两个请求现在都执行完成
-                console.info(rightC.data.data.list.data);
                 vm.newsLeftData = leftC.data.data.list.data;
+                vm.newsLeftData.forEach(function (element, index, array) {
+                    // element: 指向当前元素的值
+                    // index: 指向当前索引
+                    // array: 指向Array对象本身
+                    element.create_time = formatDate(element.create_time);
+                });
                 vm.newsRightData = rightC.data.data.list.data;
             }));
+    },
+    methods:{
+        toggleLoading(show) {
+            show = true;
+            this.loading = show
+        },
+        fakeAjax() {
+            alert(0);
+            this.toggleLoading(true);
+            setTimeout(function () {
+                this.toggleLoading(false)
+            }, 1000);
+        },
+        ready() {
+            this.fakeAjax()
+        }
     }
 })
+
 
 function getLeft() {
     return axios.get('http://192.168.0.191/home/content/newlists?category=27');
