56eedf06d407606dce7e9139fd3f605bc1ddd354
shaossss <969348836@qq.com>
2018-04-13 15:17:14 +0800

    update

--- js/newsCenter.js
+++ js/newsCenter.js
@@ -2,23 +2,20 @@ var newsCenter = new Vue({
     el:'#main',
     data:{
         loading: false,
+        keyWord:'请输入关键字',
         newsLeftData:[],
         newsRightData:[],
         all: '', //总页数
-        right:'',
+        allPage:'',
         cur: 1,//当前页码
     },
     mounted:function () {
         var vm = this;
         axios.all([getLeft(), getRight()])
-            .then(axios.spread(function (leftC, rightC) {
-                // 两个请求现在都执行完成
+            .then(axios.spread(function (leftC, rightC) {// 两个请求现在都执行完成
                 vm.newsLeftData = leftC.data.data.list.data;
                 vm.all = leftC.data.data.list.total;
-                vm.newsLeftData.forEach(function (element, index, array) {
-                    // element: 指向当前元素的值
-                    // index: 指向当前索引
-                    // array: 指向Array对象本身
+                vm.newsLeftData.forEach(function (element, index, array) {// element: 指向当前元素的值 index: 指向当前索引 array: 指向Array对象本身
                     element.create_time = formatDate(element.create_time);
                 });
                 vm.newsRightData = rightC.data.data.list.data;
@@ -38,6 +35,10 @@ var newsCenter = new Vue({
         },
         ready() {
             this.fakeAjax()
+        },
+        /*搜索*/
+        Search:function(){
+            /*写入方法*/
         },
         /*分页*/
         btnClick: function(data){//页码点击事件
@@ -61,18 +62,33 @@ var newsCenter = new Vue({
     },
     //分页
     computed: {
-        indexs: function(){
+        indexs: function() {
             var left = 1;
-              /*var right = this.all;*/
             var vm = this;
-            vm.right = this.all%5==0?this.all/5:Math.ceil(this.all/5);
+            /*总页数*/
+            vm.allPage = this.all % 5 == 0 ? this.all / 5: Math.ceil(this.all / 5);
+            var right = vm.allPage;
             var ar = [];
-            while (left <= vm.right){
-                ar.push(left);
-                left ++
+            if (vm.allPage >= 5) {
+                if (this.cur > 3 && this.cur < vm.allPage - 2) {
+                    left = this.cur - 2
+                    right = this.cur + 2
+                } else {
+                    if (this.cur <= 3) {
+                        left = 1
+                        right = 5
+                    } else {
+                        right = vm.allPage
+                        left = vm.allPage - 4
+                    }
+                }
             }
-        return ar
-       }
+            while (left <= right) {
+                ar.push(left);
+                left++;
+            }
+            return ar
+        }
          
     },
     watch: {
@@ -90,12 +106,11 @@ function getLeft(curpage) {
         url: 'http://192.168.0.191/home/content/newlists',
         params: {
             category : 2,
-            limit: 5,
+            limit:5,
             page: par
         }
     });
     return l;
-
 }
 
 function getRight() {
