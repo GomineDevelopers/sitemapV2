63a82a7b9937abcdec29fe01e7c32b2196c132e7
shaossss <969348836@qq.com>
2018-04-27 16:38:55 +0800

    update

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,49 +11,160 @@ var chartResolution = new Vue({
 
     mounted: function () {
         var vm = this;
+        vm.drawPieChart();
         vm.drawBarChart();
-        
-
-
     },
     methods:{
-        drawBarChart() {
+        drawPieChart:function(){
+            var pieChartOne = echarts.init(document.getElementById('pieBasicOne'), 'macarons');
+            var pieChartTwo = echarts.init(document.getElementById('pieBasicTwo'), 'macarons');
+            var pieChartThree = echarts.init(document.getElementById('pieBasicThree'), 'macarons');
+            var pieChartFour = echarts.init(document.getElementById('pieBasicFour'), 'macarons');
+
+            var pieOptionOne = {
+                title:{
+                    text:'注册资本',
+                    textStyle:{
+                        color:'#000'
+                    },
+                    top:'5%'
+                },
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b}: {c} ({d}%)"
+                },
+                series: [
+                    {
+                        name:'访问来源',
+                        type:'pie',
+                        radius: ['40%', '60%'],
+                        labelLine: {
+                            normal: {
+                                show: true
+                            }
+                        },
+                        data:[
+                            {value:335, name:'直接访问'},
+                            {value:310, name:'邮件营销'},
+                            {value:234, name:'联盟广告'},
+                            {value:135, name:'视频广告'},
+                            {value:1548, name:'搜索引擎'}
+                        ]
+                    }
+                ]
+            };
+            pieChartOne.setOption(pieOptionOne);
+
+            var pieOptionTwo = {
+                title:{
+                    text:'成立时间',
+                    textStyle:{
+                        color:'#000'
+                    },
+                    top:'5%'
+                },
+                tooltip : {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                series : [
+                    {
+                        name: '访问来源',
+                        type: 'pie',
+                        radius : '60%',
+                        data:[
+                            {value:335, name:'直接访问'},
+                            {value:310, name:'邮件营销'},
+                            {value:234, name:'联盟广告'},
+                            {value:135, name:'视频广告'},
+                            {value:1548, name:'搜索引擎'}
+                        ],
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            };
+            pieChartTwo.setOption(pieOptionTwo);
+            pieChartThree.setOption(pieOptionOne);
+            pieChartFour.setOption(pieOptionTwo);
+        },
+
+        drawBarChart:function () {
             // 基于准备好的dom，初始化echarts实例
-            axios.get('#')
+            /*axios.get('#')
             .then(function (response) {
                 
             })
             .catch(function (error) {
                 console.log(error);
-            });
+            });*/
 
         // 图表插入
-        var lineChart = echarts.init(document.getElementById('lineBasic'), 'macarons');
-        var barChart = echarts.init(document.getElementById('barBasic'), 'macarons');
-        var pieChartOne = echarts.init(document.getElementById('pieBasicOne'), 'macarons');
-        var pieChartTwo = echarts.init(document.getElementById('pieBasicTwo'), 'macarons');
-        var pieChartThree = echarts.init(document.getElementById('pieBasicThree'), 'macarons');
-        var pieChartFour = echarts.init(document.getElementById('pieBasicFour'), 'macarons');
-        var lineOption = {
+        var barChartR = echarts.init(document.getElementById('barBasicR'), 'macarons');
+        var barChartB = echarts.init(document.getElementById('barBasicB'), 'macarons');
+
+
+        /*右边柱状图*/
+        var BarOptionR = {
+            title:{
+                text:'行业',
+                textStyle:{
+                    color:'#000'
+                },
+                top:'3%'
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            /*grid:{
+                x:80
+            },*/
             xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                type: 'value',
+                boundaryGap: [0, 0.01]
             },
             yAxis: {
-                type: 'value'
+                type: 'category',
+                data: ['巴西','印尼','美国美国','印度','中国','皮革，毛皮，羽毛及其制品和制鞋业'],
+                axisLabel:{
+                    formatter:function (value,index) {
+                        if(value.length > 3){
+                            value = value.substr(0,3)+'...';
+                        }
+                        return value;
+                    }
+                }
             },
-            series: [{
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line'
-            }]
+            series: [
+                {
+                    type: 'bar',
+                    data: [18203, 23489, 29034, 104970, 131744, 630230]
+                }
+            ]
         };
-        lineChart.setOption(lineOption);
+        barChartR.setOption(BarOptionR);
 
 
-        var barOption = {
+        /*下面柱状图*/
+        var barOptionB = {
+            title:{
+                text:'注册地区',
+                textStyle:{
+                    color:'#000'
+                },
+                top:'3%'
+            },
             xAxis: {
                 type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
             },
             yAxis: {
                 type: 'value'
@@ -63,53 +174,9 @@ var chartResolution = new Vue({
                 type: 'bar'
             }]
         };
-        barChart.setOption(barOption);
+        barChartB.setOption(barOptionB);
 
-
-        var pieOption = {
-            title: {
-                text: '某站点用户访问来源',
-                subtext: '纯属虚构',
-                x: 'center'
-            },
-            tooltip: {
-                trigger: 'item',
-                formatter: "{a} <br/>{b} : {c} ({d}%)"
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left',
-                data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-            },
-            series: [
-                {
-                    name: '访问来源',
-                    type: 'pie',
-                    radius: '55%',
-                    center: ['50%', '60%'],
-                    data: [
-                        { value: 335, name: '直接访问' },
-                        { value: 310, name: '邮件营销' },
-                        { value: 234, name: '联盟广告' },
-                        { value: 135, name: '视频广告' },
-                        { value: 1548, name: '搜索引擎' }
-                    ],
-                    itemStyle: {
-                        emphasis: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    }
-                }
-            ]
-        };
-        pieChartOne.setOption(pieOption);
-        pieChartTwo.setOption(pieOption);
-        pieChartThree.setOption(pieOption);
-        pieChartFour.setOption(pieOption);
-          }
-      
+        }
     }
 
 })
