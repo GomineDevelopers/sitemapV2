67ed91c659c9e2f3808a7889625cf317204cb4d5
arronZhu <1274294199@qq.com>
2018-05-07 17:04:20 +0800

    change el

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ Vue.use(VueBaiduMap.default, {
     ak: 'piXQ5CgHFqEefqCVbhhBFfe6HjF7l4zW'
 });
 new Vue({
-    el: "#app",
+    el: "#mapInit",
     data: {
         zoom: 5,
         center: { lng: 116.463, lat: 39.924 }
