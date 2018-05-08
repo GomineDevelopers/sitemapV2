6eb35435542c7c3e0aa1a17d73fac53bf2ff8f9c
shaossss <969348836@qq.com>
2018-05-08 15:01:06 +0800

    update

--- js/map.js
+++ js/map.js
@@ -1,10 +1,34 @@
-Vue.use(VueBaiduMap.default, {
-    ak: 'piXQ5CgHFqEefqCVbhhBFfe6HjF7l4zW'
-});
-new Vue({
-    el: "#mapInit",
-    data: {
-        zoom: 5,
-        center: { lng: 116.463, lat: 39.924 }
-    }
-})
+/*初始化*/
+var map = new BMap.Map("container"); // 创建地图实例
+var point = new BMap.Point(116.404, 39.915);  // 创建中心点坐标
+map.centerAndZoom(point,5);                 // 初始化地图，设置中心点坐标和地图级别
+map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+console.info(point);
+
+/*添加控件*/
+map.addControl(new BMap.ScaleControl() ); //比例尺
+map.addControl(new BMap.MapTypeControl() );//地图类型
+
+addMarker();
+/*多点标注*/
+function addMarker() {
+    $.ajax({
+       url:'../js/NE.json',
+        type:'GET',
+        dataType:'json',
+        success:function (data,status) {
+            console.info(data.province);
+            for(var i = 0;i<data.province.length;i++){
+                var point = new BMap.Point(data.province[i].lng,data.province[i].lat);
+                var marker = new BMap.Marker(point);
+                map.addOverlay(marker);
+            }
+        },
+        error:function (data,status) {
+            console.info(status);
+        }
+    });
+}
+
+
+
