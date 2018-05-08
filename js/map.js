1fc79922994622aa1d85a74e30faf8f4d9e537c8
shaossss <969348836@qq.com>
2018-05-08 15:01:57 +0800

    update

--- js/map.js
+++ js/map.js
@@ -3,7 +3,6 @@ var map = new BMap.Map("container"); // 创建地图实例
 var point = new BMap.Point(116.404, 39.915);  // 创建中心点坐标
 map.centerAndZoom(point,5);                 // 初始化地图，设置中心点坐标和地图级别
 map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-console.info(point);
 
 /*添加控件*/
 map.addControl(new BMap.ScaleControl() ); //比例尺
