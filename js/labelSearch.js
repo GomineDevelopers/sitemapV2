807512992c21b9568322b433842401661b5f8e03
shaossss <969348836@qq.com>
2018-05-10 18:09:41 +0800

    update

--- js/labelSearch.js
+++ js/labelSearch.js
@@ -38,6 +38,11 @@ var app = new Vue({
             });
     },
     methods: {
+        goChart:function(){
+            var vm = this;
+            var temp = "type="+vm.type+"&info="+vm.info+"&organizor="+vm.organizor;
+            window.location.href = './dashboard.html?'+temp;
+        },
         /*分页*/
         btnClick: function (data) {//页码点击事件
             var vm = this;
